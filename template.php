37ee4d8903a5ee1d4f251dcbfa3f0e6ac34abd75
Bruno Corrêa  Sena (Brubineu) <brubineubrubineia@proton.me>
2022-09-02 16:39:11 -0300

    Fixed social media links alignment code.

--- template.php
+++ template.php
@@ -1,5 +1,5 @@
 <?php
-include 'includes/head.inc.php';
+    include 'includes/head.inc.php';
 ?>
 
 <!DOCTYPE html>
@@ -12,6 +12,7 @@ include 'includes/head.inc.php';
 
 <body>
     <div class="container">
+        <!-- Header menu (optional) -->
         <header>
             <nav>
                 <ul>
@@ -23,29 +24,31 @@ include 'includes/head.inc.php';
             <div class="page-wrap"></div>
         </main>
         <!-- Footer (optional) -->
-        <footer>
-            <!-- Site Links -->
-            <section>
-                <ul>
-                    <li>
-                        <h3><a href="#"></a></h3>
-                    </li>
-                    <li><a href="#"></a></li>
-                </ul>
-            </section>
-            <!-- Social Media Links (optional) 
-        <section>
-            <ul>
-                <li>
-                    <h3><a href="#"></a></h3>
-                </li>
-                <li><a href="#"></a></li>
-            </ul>
-        </section>
-        -->
-        </footer>
+  <footer>
+    <!-- Site Links -->
+    <section class="section-1">
+      <ul>
+        <li>
+          <h3><a href="#">Title</a></h3>
+        </li>
+        <li><a href="#">Link</a></li>
+        <li><a href="#">Link</a></li>
+        <li><a href="#">Link</a></li>
+        <li><a href="#">Link</a></li>
+        <li><a href="#">Link</a></li>
+      </ul>
+    </section>
+    <!-- Social Media Links -->
+    <section class="section-2">
+      <ul>
+        <li><a href="#">Twitter</a></li>
+        <li><a href="#">Reddit</a></li>
+        <li><a href="#">Instagram</a></li>
+        <li><a href="#">Linkedin</a></li>
+      </ul>
+    </section>
+  </footer>
     </div>
-    <!-- Header menu (optional) -->
 </body>
 
 </html>
